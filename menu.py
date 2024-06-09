aefc048c05c2f056f53ce4230d69479afb510b5f
junyi1616 <junzhu1616@gmail.com>
2024-06-09 05:51:22 -0700

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,16 @@
+# This file contains the menu information for the restaurant chatbot.
+
+menu = {
+    "Sweet and Sour Pork": 12.5,
+    "Kung Pao Chicken": 10.0,
+    "Ma Po Tofu": 8.5,
+    "Spring Rolls": 4.0,
+    "Dumplings": 6.0,
+    "Fried Rice": 7.5,
+    "Hot and Sour Soup": 5.0,
+    "Wonton Soup": 6.5,
+    "Peking Duck": 25.0
+}
+
+def get_menu():
+    return menu
